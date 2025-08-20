e0e823dbd40879cb1f8ce4702838db503ce26698
dipankor99 <dipankor.gratia@gmail.com>
2025-08-20 16:58:15 +0530

    add react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
 // import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navigation from './components/Navigation'
+import Signin from './components/Signin'
+import Box from './components/Box';
+import DraggableCard from './components/DraggableCard'
 import Home from './components/Home'
 function App() {
+function Contact() {
+  return <h1>Contact Page</h1>;
+}
 
   return (
     <>
-    <Navigation/>
-      <Home/>
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<Box />} />
+        <Route path="/products" element={<DraggableCard />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
+    </Router>
     </>
   )
 }
